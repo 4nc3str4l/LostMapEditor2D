4cf6be9827d22420cb9bf1a78fc1e1a1671414a4
4nc3str4l <cmuri_10@hotmail.com>
2019-05-05 16:19:49 +0200

    Show delta time in console

--- src/lost_map.cpp
+++ src/lost_map.cpp
@@ -1,5 +1,10 @@
 #include "lost_map.h"
 
+#include "le/tools/log.h"
+#include "le/tools/timer.h"
+
+using namespace le;
+
 LostMapEditor::LostMapEditor(const std::string& title, int width, int height):
     Application(title, width, height)
 {
@@ -11,9 +16,9 @@ void LostMapEditor::init(le::Window* _window)
 
 }
 
-void LostMapEditor::update(float _delta, le::Window* _window)
+void LostMapEditor::update(le::Window* _window)
 {
-
+    LOG_FAIL(le::Timer::deltaTime);
 }
 
 void LostMapEditor::render(le::Window* _window)
--- src/lost_map.h
+++ src/lost_map.h
@@ -8,6 +8,6 @@ class LostMapEditor: public le::Application
 public:
     LostMapEditor(const std::string& title, int width, int height);
     void init(le::Window* _window);
-    void update(float _delta, le::Window* _window);
+    void update(le::Window* _window);
     void render(le::Window* _window);
 }; 
