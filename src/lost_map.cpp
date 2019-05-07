15f9fcc8fa5e4954abcdff491ae12beda13601bb
Cristian Muriel Ordoñez <cristianmurielordonez@MacBook-Pro-de-Cristian.local>
2019-05-07 16:25:36 +0200

    Map Editor and engine building for WINDOWS MAC AND LINUX (Crossplatform !!!)

--- src/lost_map.cpp
+++ src/lost_map.cpp
@@ -2,26 +2,25 @@
 
 #include "le/tools/log.h"
 #include "le/tools/timer.h"
+#include "le/tools/event_manager.h"
+#include <functional>
 
 using namespace le;
 
-LostMapEditor::LostMapEditor(const std::string& title, int width, int height):
-    Application(title, width, height)
+LostMapEditor::LostMapEditor(const std::string& title, int width, int height): Application(title, width, height)
 {
-
 }
+
 
 void LostMapEditor::init(le::Window* _window)
 {
-
+    le::EventManager::subscribe(le::Event::drop_file, []() { LOG_SPECIAL("Received Drop!"); });
 }
 
 void LostMapEditor::update(le::Window* _window)
 {
-    LOG_FAIL(le::Timer::deltaTime);
 }
 
 void LostMapEditor::render(le::Window* _window)
 {
-
 }
