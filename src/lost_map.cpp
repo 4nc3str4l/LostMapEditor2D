1806e84bbcd973145ddbb352e35f72eb6fd961ea
4nc3str4l <murielmaths@gmail.com>
2019-05-09 21:08:42 +0200

    Drop event handling

--- src/lost_map.cpp
+++ src/lost_map.cpp
@@ -2,19 +2,29 @@
 
 #include "le/tools/log.h"
 #include "le/tools/timer.h"
+#include "le/tools/loader.h"
 #include "le/tools/event_manager.h"
+#include "le/tools/input.h"
 #include <functional>
 
 using namespace le;
 
-LostMapEditor::LostMapEditor(const std::string& title, int width, int height): Application(title, width, height)
-{
-}
-
+LostMapEditor::LostMapEditor(const std::string& title, int width, int height): Application(title, width, height){}
 
 void LostMapEditor::init(le::Window* _window)
 {
-    le::EventManager::subscribe(le::Event::drop_file, []() { LOG_SPECIAL("Received Drop!"); });
+    auto shader = le::Loader::shader("res/shaders/sprite");
+    EventManager::onFileDropped.Bind<LostMapEditor, &LostMapEditor::onFileDropped>(this);
+
+    // Configure shaders
+    LOG_CORRECT(width, ", ", height);
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(width), static_cast<GLfloat>(height), 0.0f, -1.0f, 1.0f);
+    
+	shader->Use();
+	shader->SetInt("image", 0);
+    shader->SetMat4("projection", projection);
+    shader->SetVec2("light_pos", glm::vec2(0, 0));
+    spriteRenderer = new SpriteRenderer(*shader);
 }
 
 void LostMapEditor::update(le::Window* _window)
@@ -23,4 +33,23 @@ void LostMapEditor::update(le::Window* _window)
 
 void LostMapEditor::render(le::Window* _window)
 {
+    spriteRenderer->Render(sprites);
+}
+
+void LostMapEditor::onFileDropped(const std::string& path)
+{
+    LOG_INFO("Dropped file: ", path);
+    auto texture = le::Loader::LoadTexture(path, true);
+    LOG_INFO(Input::MousePosX, Input::MousePosY);
+    sprites.push_back(new le::Sprite(Input::MousePosX, Input::MousePosY, texture));
+}
+
+LostMapEditor::~LostMapEditor()
+{
+    for(Sprite* s : sprites)
+    {
+        delete s;
+    }
+
+    delete spriteRenderer;
 }
--- src/lost_map.h
+++ src/lost_map.h
@@ -2,12 +2,20 @@
 
 #include "le/logic/application.h"
 #include "le/gfx/window.h"
+#include "le/gfx/sprite_renderer.h"
+#include <vector>
 
 class LostMapEditor: public le::Application
 {
 public:
     LostMapEditor(const std::string& title, int width, int height);
+    ~LostMapEditor();
     void init(le::Window* _window);
     void update(le::Window* _window);
     void render(le::Window* _window);
+private:
+    void onFileDropped(const std::string& path);
+private:
+    std::vector<le::Sprite*> sprites;
+    le::SpriteRenderer* spriteRenderer;
 }; 
